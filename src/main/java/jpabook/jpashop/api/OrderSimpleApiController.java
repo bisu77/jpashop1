bd376fe363f6da5901b1624d616fa73aa552d8d8
DEV_YKJ <rudwns7552@gmail.com>
2021-03-06 00:22:36 +0900

    1. 주문관련 (배송+고객) API 조회 기능 추가.V1    => jsonToString으로 인한 무한루프 현상 발생 2. DB샘플 등록(InitDb.java) 추가 3. yml 수정 none => create

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        List<Order> orders = orderService.findOrders(new OrderSearch());
+        return orders;
+    }
+}
