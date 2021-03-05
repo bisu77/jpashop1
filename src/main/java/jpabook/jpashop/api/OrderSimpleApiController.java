33b420f3c402f0b3946898507cd692e0fca23365
DEV_YKJ <rudwns7552@gmail.com>
2021-03-06 00:43:35 +0900

    * 주문관련 (배송+고객) API 조회 기능 추가.V1    1) jsonToString으로 인한 무한루프 현상 발생       => @JsonIgnore 추가로 무한루프 현상 해결
       2) 그러나, 지연로딩(Lazy)으로 인한 Member, Delivery 객체는 Proxy객체로 세팅되기 때문에 JSON 파싱 불가, Type definition error 오류 발생
          => hibernate5 모듈 추가 후 Bean 등록 해서 오류 해결
    
       3) 그러나, hibernate5 모듈은 초기화 되지 않은 Proxy객체는 null로 반환해 원하는 Member, Delivery 정보를 얻지 못함
          => hibernate5 모듈에 지연로딩 강제실행 해서 null이 아닌 원하는 데이터 반환
    
       4) 그러나, 이렇게 문제를 해결할 경우 모든 Lazy로딩이 강제 실행되어서, 성능최적화 및 API 스펙기준을 맞출 수가 없음
          => Lazy로딩의 특징인 해당 객체의 요소를 get하여서 해당 Lazy지연로딩을 호출해 문제 해결
    
       5) V1의 문제는 해결했으나, 엔티티 반환 시 발생하는 많은 문제점은 아직 해결되지 않음.!

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1(){
         List<Order> orders = orderService.findOrders(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
         return orders;
     }
 }
