ed31b680872a80b2f766599e0fd068fba047bef4
DEV_YKJ <rudwns7552@gmail.com>
2021-03-06 00:51:46 +0900

    * 주문관련 (배송+고객) API 조회 기능 추가.V1    1) jsonToString으로 인한 무한루프 현상 발생       => @JsonIgnore 추가로 무한루프 현상 해결
       2) 그러나, 지연로딩(Lazy)으로 인한 Member, Delivery 객체는 Proxy객체로 세팅되기 때문에 JSON 파싱 불가, Type definition error 오류 발생
          => hibernate5 모듈 추가 후 Bean 등록 해서 오류 해결
    
       3) 그러나, hibernate5 모듈은 초기화 되지 않은 Proxy객체는 null로 반환해 원하는 Member, Delivery 정보를 얻지 못함
          => hibernate5 모듈에 지연로딩 강제실행 해서 null이 아닌 원하는 데이터 반환
    
       4) 그러나, 이렇게 문제를 해결할 경우 모든 Lazy로딩이 강제 실행되어서, 성능최적화 및 API 스펙기준을 맞출 수가 없음
          => Lazy로딩의 특징인 해당 객체의 요소를 get하여서 해당 Lazy지연로딩을 호출해 문제 해결
    
       5) V1의 문제는 해결했으나, 엔티티 반환 시 발생하는 많은 문제점은 아직 해결되지 않음.!
    
       *** PS:: 즉시로딩(EAGER)로 변경해서 사용할 경우 해당 문제가 해결되기는 하나, 많은 문제점을 야기 시킬 수 있음
          1) 성능최적화 되지 않음(LAZY와 동일한 SQL실행)
             => em.find를 통해 key값을 넘기는 방식이면 Join을 통해 1쿼리로 가져올 수 있지만
                에제와 같이 JPQL을 통해 넘기는 방식이면 (ex : select o.* from Order o)
                JPQL 실행 후 Member, Delivery 객체 쿼리가 실행 되기 때문에 성능최적화가 되지 않음
                => 성능 최적화 하려면 페치조인을 사용해야 함.!
    
          2) 공통 엔티티에 물려있으므로 다른 API, 다른 서비스에 수 많은 사이드 이펙트 발생
             => xToOne은 절~~대 EAGER 설정하지 않기.!

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -22,14 +22,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne(fetch = EAGER)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = EAGER, cascade = CascadeType.ALL)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
