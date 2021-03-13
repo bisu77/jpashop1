4c18f0cfeb8e868c03b987462c3137e4155c3abc
DEV_YKJ <rudwns7552@gmail.com>
2021-03-14 00:44:11 +0900

    컬렉션 조회 V4 개발 => 컬렉션 조회 최적화(1+1 쿼리 조회)    OrderQueryRepository.findAllByDto_optimization 추가        1) Order 페치 조인 조회        2) 1번 리턴 값 List<OrderItemDto> -> List<OrderItemDto.OrderId> 변환        3) OrderItem IN 쿼리 조회(1번)        4) 3번 리턴 값 Map<Order.OrderId, List>으로 담기        5) result 루프 돌면서 OrderId에 맞는 OrderItemDto 세팅

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -25,19 +25,19 @@ public class OrderApiController {
     private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
-    public List<Order> orderV1(){
+    public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName();
             order.getDelivery().getAddress();
             List<OrderItem> orderItems = order.getOrderItems();
-            orderItems.stream().forEach(o->o.getItem().getName());
+            orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
     }
 
     @GetMapping("/api/v2/orders")
-    public List<OrderDto> orderV2(){
+    public List<OrderDto> orderV2() {
         List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
@@ -47,7 +47,7 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> orderV3(){
+    public List<OrderDto> orderV3() {
         List<Order> findOrder = orderRepository.findAllWithItem();
         List<OrderDto> result = findOrder.stream()
                 .map(OrderDto::new)
@@ -57,7 +57,7 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> orderV3_page(@RequestParam(value = "offset") int offset, @RequestParam(value="limit") int limit){
+    public List<OrderDto> orderV3_page(@RequestParam(value = "offset") int offset, @RequestParam(value = "limit") int limit) {
         List<Order> findOrder = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = findOrder.stream()
                 .map(OrderDto::new)
@@ -67,9 +67,10 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v4/orders")
-    public List<OrderQueryDto> orderV4(){
+    public List<OrderQueryDto> orderV4() {
         return orderQueryRepository.findOrderQueryDtos();
     }
+
     @Data
     static class OrderDto {
 
@@ -90,6 +91,11 @@ public class OrderApiController {
                     .map(orderItem -> new OrderItemDto(orderItem))
                     .collect(Collectors.toList());
         }
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -38,5 +40,38 @@ public class OrderQueryRepository {
                 OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
                 .getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));//컬렉션 1쿼리 조회 후 Map 형태 반환
+
+        result.forEach(o->o.setOrderItems(orderItemMap.get(o.getOrderId())));//루프 돌면서 컬렉션 세팅
+
+        return result;
+    }
+
+    //1) IN쿼리 수행 후,
+    //2) Map<OrderItem.Order.Id, List<OrderItemQueryDto>> 형태<key,List> 반환
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItemQueryDto = em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id IN :orderIds"
+                , OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItemQueryDto.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    //List<OrderItemDto> -> List<OrderItemDto.OrderId>
+    private List<Long> toOrderIds(List<OrderQueryDto> orders) {
+        return orders.stream()
+                    .map(o -> o.getOrderId())
+                    .collect(Collectors.toList());
     }
 }
